2c458791506f99613b29493e4190f1382d5f0549
Manoel Vieira <vieiranetoc@gmail.com>
2017-01-26 18:25:55 -0200

    increased newton-ralphson precision

--- Octave/Lista1/nwtrph.m
+++ Octave/Lista1/nwtrph.m
@@ -2,8 +2,8 @@ function root = nwtrph(x)
     syms y;
     g = y^2+y-6;
     gl = diff(g);
-    root = x - vpa(subs(g, y, x))/vpa(subs(gl, y, x))
-    if abs(root-x) < 10^-3
+    root = x - vpa(subs(g, y, x))/vpa(subs(gl, y, x));
+    if abs(root-x) < 10^-5
         return
     else
         root = nwtrph(root);
